3080f2591bbe7bbd22cb5472f79f520fe97db688
Piotr Kozlowski <pior@obrandowani.pl>
2018-11-09 17:29:21 +0100

    show status of servers

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,19 +1,67 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link to="/" :active="'active'">Home</router-link>
-      </li>
-    </ul>
-  </nav>
+  <div class="top-bar">
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/" :active="'active'">Home</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="servers-status">
+      <span class="title">Server status:</span>
+      <span class="server">Defaults elements: <status-icon :status="statusServerDefaults" /></span>
+      <span class="server">Custom elements: <status-icon :status="statusServerCustom" /></span>
+    </div>
+  </div>
 </template>
 
+<script>
+import StatusIcon from '../Jobs/StatusIcon/StatusIcon'
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      statusServerDefaults: 'pending',
+      statusServerCustom: 'pending',
+    }
+  },
+  components: {
+    StatusIcon
+  },
+  beforeCreate () {
+    axios.get('https://content-finder.herokuapp.com/')
+      .then(res => {
+        this.statusServerDefaults = 'active'
+      })
+      .catch(e => {
+        this.statusServerDefaults = 'error'
+      })
+
+    axios.get('http://pistacja.kylos.net.pl:3000/')
+      .then(res => {
+        this.statusServerCustom = 'active'
+      })
+      .catch(e => {
+        this.statusServerCustom = 'error'
+      })
+  }
+}
+</script>
+
+
 <style lang="scss" scoped>
+.top-bar {
+  display: flex;
+  align-items: center;
+  margin-bottom: 50px;
+}
+
 nav {
   height: 60px;
   border-bottom: 1px solid #ddd;
   text-align: left;
-  margin-bottom: 50px;
+  margin-right: 50px;
   ul {
     height: 100%;
     margin: 0;
@@ -36,6 +84,22 @@ nav {
           background-color: #2c3e50;
         }
       }
+    }
+  }
+}
+.servers-status {
+  display: flex;
+  .title {
+    font-weight: 700;
+    margin-right: 20px;
+  }
+  .server {
+    margin-right: 20px;
+    display: flex;
+    align-items: center;
+    .status-icon {
+      margin-left: 5px;
+      width: 20px;
     }
   }
 }
